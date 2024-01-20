236cf0f20053d4964dfdb7fee7c15469fda393db
Feraz Khan <ferazahmadgit62@gmail.com>
2024-01-20 21:24:49 +0530

    cors pkg added

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const TicketModel = require("./Models/ticketModel.js");
 const sequelize = require("./utils/database.js");
 const ticketRouter = require("./routes/ticketRouter.js");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api/v1/ticket", ticketRouter);
